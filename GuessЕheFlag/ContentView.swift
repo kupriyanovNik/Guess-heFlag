f28803dc4a1c251470407ab3f189a1f8902f0a43
kupriyanovNik <cucuprianov@icloud.com>
2023-07-26 22:46:28 +0300

    Completed GuessTheFlag + Day34 challenge

--- GuessЕheFlag/ContentView.swift
+++ GuessЕheFlag/ContentView.swift
@@ -8,16 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var showingScore: Bool = false
-    @State private var scoreTitle: String = ""
-    
-    @State private var currentQuestion: Int = 0
-    @State private var score: Int = 0
-    
-    @State private var isEnded: Bool = false
-    
-    @State private var counties: [String] = [
+    @State private var showAnimation = false
+    @State private var mainAnimationValue = 0.0
+    @State private var countries = [
         "Estonia",
         "France",
         "Germany",
@@ -28,113 +21,146 @@ struct ContentView: View {
         "Russia",
         "Spain",
         "UK",
-        "US"
+        "US",
     ].shuffled()
-    @State private var currentQuestionCounties: [String] = []
-    @State private var correctAnswer: String = ""
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore: Bool = false
+    @State private var isGameEnded: Bool = false
+    @State private var scoreTitle: String = ""
+    @State private var score: Int = 0
+    @State private var currentQuestionNumber: Int = 1
+    @State private var selectedId = ""
     
     var body: some View {
-        
         ZStack {
             AnimatedGradient()
+            
             VStack {
-                
-                Spacer()
-                
-                Text("Guess the flag")
-                    .font(.largeTitle.bold())
-                    .foregroundColor(.white)
+                Text("Guess The Flag")
+                    .makeLargeAndBlue()
                 VStack(spacing: 30) {
-                    
                     VStack {
                         Text("Tap the flag of")
                             .font(.subheadline.weight(.heavy))
                             .foregroundStyle(.secondary)
-                        Text(correctAnswer)
+                        Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
-                        
-                    
-                    ForEach(currentQuestionCounties, id: \.self) { country in
-                        Button {
-                            flagTapped(country)
-                        } label: {
-                            Image(country)
-                                .renderingMode(.original)
-    //                            .clipShape(Capsule())
-                                .cornerRadius(10)
-                                .shadow(radius: 5)
+                    ForEach(0..<3) { number in
+                        FlagImage(image: countries[number]) { flagID in
+                            withAnimation(.easeOut(duration: 0.5)) {
+                                
+                                self.showAnimation = true
+                            }
+                            flagTapped(number)
                         }
-
+                        .rotation3DEffect(.degrees(number == self.correctAnswer && self.showAnimation ? 360 : 0.0), axis: (x: 0, y: 1, z: 0))
+                        .opacity(number != self.correctAnswer && self.showAnimation ? 0.75 : 1)
+                        .scaleEffect(number != self.correctAnswer && self.showAnimation ? 0.75 : 1)
                     }
                 }
-                .onAppear {
-                    askQuestion()
-                }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 20)
-                .background(.regularMaterial)
-                .cornerRadius(10)
-//            .padding(.horizontal)
+                .foregroundColor(.white)
+                .padding(40)
+                .padding(.horizontal, 25)
+                .background(.ultraThinMaterial)
+                .cornerRadius(15)
                 
-                Spacer()
-                
-                Text("Score: \(score)")
+                Text("My score: \(score)")
+                    .font(.largeTitle.weight(.bold))
                     .foregroundColor(.white)
-                    .font(.title.bold())
-                
-                Spacer()
-                
+                Text("Question number: \(currentQuestionNumber)")
+                    .font(.subheadline.weight(.heavy))
+                    .foregroundStyle(.secondary)
             }
-            .padding( )
-            
         }
-        .alert("Game ended", isPresented: $isEnded) {
-            Button("New game", action: newGame)
-        } message: {
-            Text("Your score: \(score)/10")
+        .animation(.linear, value: selectedId)
+        .onAppear {
+            countries.shuffle()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
             Text("Your score is \(score)")
         }
-    }
-    
-    func flagTapped(_ country: String) {
-        withAnimation {
-            self.currentQuestion += 1
-            switch country {
-            case correctAnswer:
-                self.scoreTitle = "Correct"
-                self.score += 1
-            default:
-                self.scoreTitle = "Wrong"
-            }
-            self.showingScore = true
-            if self.currentQuestion == 10 {
-                self.isEnded = true
-            }
+        .alert("Game ended", isPresented: $isGameEnded) {
+            Button("New game", action: reset)
+        } message: {
+            Text("Your score: \(score)/10")
         }
     }
-    func askQuestion() {
-        self.currentQuestionCounties = [
-            self.counties.randomElement()!,
-            self.counties.randomElement()!,
-            self.counties.randomElement()!
-        ]
-        self.correctAnswer = currentQuestionCounties.randomElement()!
+    
+    func flagTapped(_ number: Int) {
+        selectedId = ""
+        currentQuestionNumber += 1
+        if number == correctAnswer {
+            scoreTitle = "Cool."
+            score += 1
+        } else {
+            scoreTitle = "Wrong."
+        }
+        showingScore = true
+        if currentQuestionNumber == 10 {
+            isGameEnded = true
+        }
     }
-    func newGame() {
-        self.score = 0
-        self.currentQuestion = 0
-        self.counties.shuffle()
-        self.correctAnswer = currentQuestionCounties.randomElement()!
+    
+    func askQuestion() {
+        showAnimation = false
+        mainAnimationValue = 0
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        askQuestion()
+        score = 0
+        currentQuestionNumber = 0
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct FlagImage: View {
+    let id = UUID().uuidString
+    let image: String
+    @State private var ownOpacity: Double = 1
+    let callback: (String) -> ()
+    var body: some View {
+        Image(image)
+            .renderingMode(.original)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+            .onTapGesture {
+                callback(id)
+            }
+//            .gesture(
+//                DragGesture(minimumDistance: 0)
+//                    .onChanged { _ in ownOpacity = 0.5 }
+//                    .onEnded { _ in
+//                        ownOpacity = 1
+//                        callback(id)
+//                    }
+//            )
+    }
+}
+
+
+struct LargeAndBlueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func makeLargeAndBlue() -> some View {
+        modifier(LargeAndBlueTitle())
     }
 }
